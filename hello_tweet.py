6864d924118f0c24f7d738c7f8e95f20513a6c1c
carwa77 <31899683+carwa77@users.noreply.github.com>
2019-12-14 13:02:52 +0100

    Update hello_tweet.py

--- hello_tweet.py
+++ hello_tweet.py
@@ -2,8 +2,8 @@ from twython import Twython
 
 APP_KEY='w4
 APP_SECRET='
-OAUTH_TOKEN='94840857-Rd0D76KqXaW27rsbH'
-OAUTH_TOKEN_SECRET='iiA89B3Jw4X'
+OAUTH_TOKEN='94840857-Rd0rsbH'
+OAUTH_TOKEN_SECRET='iiA89X'
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
 twitter.update_status(status='Second tweet from python')
